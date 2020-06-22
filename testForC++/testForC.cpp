f8bc92faca34941e1d45d044eff9174150759a21
MichaelForwork <molinliacount@gmail.com>
2020-06-22 11:38:52 +0800

    a test C++ file.

---
+++ testForC++/testForC.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+/*
+多行注释
+this is for poeple to use template 
+*/
+
+//自定义全局常量= 宏常量
+#define variable 10
+
+int main(){
+    // 程序的入口 ，有且仅有一个main function 引入变量 variable 
+    const int c = 234;
+    int addsafasdf = 233232423;
+    double adfadf = 2342349.34234;
+    short int asdaf = 3343;
+    int a = 1;
+    int b = 2;
+    int terminal = 123;
+    string d = "dasf dsf "; // update for hello world 
+    cout << a << "= hello world" << endl;
+    return 0;
+}
